51930237358489ba310bbcfc9d24845214d9681e
Juan Camilo Morales <camov1209@gmail.com>
2018-11-29 01:02:51 -0500

    Sets up dotenv

--- src/App.js
+++ src/App.js
@@ -4,12 +4,17 @@ import './App.css';
 
 class App extends Component {
   render() {
+    
+    const author = process.env.REACT_APP_AUTHOR;
+
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
+            <br/>
+            Created by {author}
           </p>
           <a
             className="App-link"
--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+const ENV = require('dotenv')
+ENV.config()
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
